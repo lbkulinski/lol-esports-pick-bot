8e1068d3ad96eea377696647fcbbbbd61484ef1f
Logan Kulinski <lbkulinski@gmail.com>
2025-11-16 15:12:17 -0600

    GitHub Action fix

--- src/main/java/net/lbku/config/FunctionConfiguration.java
+++ src/main/java/net/lbku/config/FunctionConfiguration.java
@@ -1,5 +1,6 @@
 package net.lbku.config;
 
+import com.rollbar.notifier.Rollbar;
 import net.lbku.service.PostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,16 +15,32 @@ public class FunctionConfiguration {
     private static final Logger log = LoggerFactory.getLogger(FunctionConfiguration.class);
 
     private final PostService postService;
+    private final Rollbar rollbar;
 
     @Autowired
-    public FunctionConfiguration(PostService postService) {
+    public FunctionConfiguration(
+        PostService postService,
+        Rollbar rollbar
+    ) {
         this.postService = postService;
+        this.rollbar = rollbar;
     }
 
     @Bean
     public Supplier<String> gamePoster() {
         return () -> {
-            postService.postNewGames();
+            try {
+                this.postService.postNewGames();
+            } catch (Exception e) {
+                String message = "An error occurred while posting new games";
+
+                log.error(message, e);
+
+                this.rollbar.error(e, message);
+
+                return "ERROR";
+            }
+
             return "OK";
         };
     }
